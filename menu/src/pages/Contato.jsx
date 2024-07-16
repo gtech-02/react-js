88dabdebd41bc09cf7a1cf968b5064f23c696a99
techcomkathia <techcomk@gmail.com>
2024-07-16 12:15:37 -0300

    Create menu and routes

---
+++ menu/src/pages/Contato.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contato = () => {
+    return (
+        <div>
+            <h1>Contato</h1>
+            <p>Lista Contatos</p>
+        </div>
+    )
+}
+
+export default Contato
---
+++ menu/src/pages/Produtos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Produtos = () => {
+    return (
+        <div>
+            <h1>Produtos</h1>
+            <p>Lista dos produtos</p>
+        </div>
+    )
+}
+
+export default Produtos
