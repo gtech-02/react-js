d8ba821ac8eeef6f5e265ea7b5359ad9103069f4
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-16 10:05:45 -0300

    Iniciando componente layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+
+export default function Layout () {
+    return (
+        <>
+            <header>
+                HEADER
+            </header>
+            <main>
+                CHILDRENS
+            </main>
+            <footer>
+                FOOTER
+            </footer>
+        </>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+import Layout from '../components/Layout';
+
+export default function Home () {
+    return (
+        <>
+            <Layout>
+                <h1>H1</h1>
+            </Layout>
+        </>
+    )
+}
